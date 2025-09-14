a8b3595aea41524b3e2abd1f6e58f3985f51dbf9
sehgalaayu <119155282+sehgalaayu@users.noreply.github.com>
2025-09-14 22:43:20 +0530

    initialised db

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,14 @@
 import mongoose = require("mongoose");
 
-
-export const connectDb = async() =>{
-    try{
-        const mongoURL = process.env.MONGO_URL;
+export const connectDb = async () => {
+  try {
+    const mongoURL = process.env.MONGO_URL;
+    if (!mongoURL) {
+      throw new Error("MONGO_URL environment variable is not defined");
     }
-}
+    await mongoose.connect(mongoURL);
+    console.log("MongoDB connection successful");
+  } catch (error) {
+    console.error("Mongodb connection successful");
+  }
+};
