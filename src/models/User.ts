d0a9c391544cf17ff0e80630b2b011a723ce4c5d
sehgalaayu <119155282+sehgalaayu@users.noreply.github.com>
2025-09-17 18:41:31 +0530

    Creating more comments

--- src/models/User.ts
+++ src/models/User.ts
@@ -3,9 +3,9 @@ import bcrypt from "bcryptjs";
 import { string } from "zod";
 
 interface User {
-  username: String;
-  email: String;
-  password: String;
+  username: string; //string is the correct primitive string type used in most typings. String refers to the String object, which has many additional methods.
+  email: string;
+  password: string;
 }
 
 const userSchema = new Schema<User>(
@@ -26,6 +26,16 @@ const userSchema = new Schema<User>(
     },
   },
   {
-    timestamps: true,
+    timestamps: true, // this will create updatedAt and createdAt fields automatically
   }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  } catch (error) {
+    next(error as Error); // The "as" keyword tells the TypeScript compiler: "Treat the variable error as the type Error, no matter what else the compiler thinks."
+  }
+});
