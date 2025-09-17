eff14e6be29bc000344fd106f5930c4d99cdd870
sehgalaayu <119155282+sehgalaayu@users.noreply.github.com>
2025-09-17 18:08:53 +0530

    Creating User Schema

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,17 +1,31 @@
-import mongoose = require("mongoose");
+// import mongoose = require("mongoose");
+
+// export const connectDb = async () => {
+//   try {
+//     // In your code, you were getting the database URL from the environment variable process.env.MONGO_URL.
+//     const mongoURL = process.env.MONGO_URL;
+//     // But TypeScript says: "Hey, this variable might be empty or undefined,
+//     // but the mongoose.connect function needs a real string." So, before using mongoURL, you should check if it has a value. If it doesn't, show an error message.
+//     if (!mongoURL) {
+//       throw new Error("MONGO_URL environment variable is not defined");
+//     }
+//     await mongoose.connect(mongoURL);
+//     console.log("MongoDB connection successful");
+//   } catch (error) {
+//     console.error("Mongodb connection successful");
+//   }
+// };
+import mongoose from "mongoose";
 
 export const connectDb = async () => {
   try {
-    // In your code, you were getting the database URL from the environment variable process.env.MONGO_URL.
-    const mongoURL = process.env.MONGO_URL;
-    // But TypeScript says: "Hey, this variable might be empty or undefined,
-    // but the mongoose.connect function needs a real string." So, before using mongoURL, you should check if it has a value. If it doesn't, show an error message.
+    const mongoURL = String(process.env.MONGO_URL);
     if (!mongoURL) {
-      throw new Error("MONGO_URL environment variable is not defined");
+      console.log("mogno url is undefined");
     }
     await mongoose.connect(mongoURL);
-    console.log("MongoDB connection successful");
+    console.log(`Mongo connection successful`);
   } catch (error) {
-    console.error("Mongodb connection successful");
+    console.error("mongo connection unsuccessful");
   }
 };
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,6 @@
 import mongoose, { Schema, model } from "mongoose";
 import bcrypt from "bcryptjs";
+import { string } from "zod";
 
 interface User {
   username: String;
@@ -7,4 +8,24 @@ interface User {
   password: String;
 }
 
-const userSchema = new Schema<User>();
+const userSchema = new Schema<User>(
+  {
+    username: {
+      required: true,
+      unique: true,
+      type: String,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
